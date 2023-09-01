1492cee937322f25af71ed25d56a75651170e2ae
Santiago Galella <santi.galella@gmail.com>
2023-09-01 20:31:49 +0200

    Update IterativePrisionerDilemma

--- evolution_of_cooperation/iterative_pd.py
+++ evolution_of_cooperation/iterative_pd.py
@@ -8,7 +8,6 @@ class IterativePrisionerDilemma:
         self.payoff_CD = payoff_CD
         self.payoff_DC = payoff_DC
         self.payoff_DD = payoff_DD
-        self.history = np.nan * np.ones((self.N, 3))
 
     def _get_outcome(self, move_A, move_B):
         if move_A == 0 and move_B == 0:
@@ -20,24 +19,34 @@ class IterativePrisionerDilemma:
         elif move_A == 1 and move_B == 1:
             return 3
 
-    def _compute_payoff(self, player_A, player_B):
-        player_A.payoff = (self.payoff_CC * np.sum(self.history[:, 2] == 0)
-                           + self.payoff_CD * np.sum(self.history[:, 2] == 1)
-                           + self.payoff_DC * np.sum(self.history[:, 2] == 2)
-                           + self.payoff_DD * np.sum(self.history[:, 2] == 3))
+    def _compute_payoff(self, outcomes):
+        payoff_player_A = (self.payoff_CC * np.sum(outcomes == 0)
+                           + self.payoff_CD * np.sum(outcomes == 1)
+                           + self.payoff_DC * np.sum(outcomes == 2)
+                           + self.payoff_DD * np.sum(outcomes == 3))
 
-        player_B.payoff = (self.payoff_CC * np.sum(self.history[:, 2] == 0)
-                           + self.payoff_CD * np.sum(self.history[:, 2] == 2)
-                           + self.payoff_DC * np.sum(self.history[:, 2] == 1)
-                           + self.payoff_DD * np.sum(self.history[:, 2] == 3))
+        payoff_player_B = (self.payoff_CC * np.sum(outcomes == 0)
+                           + self.payoff_CD * np.sum(outcomes == 2)
+                           + self.payoff_DC * np.sum(outcomes == 1)
+                           + self.payoff_DD * np.sum(outcomes == 3))
+
+        return [payoff_player_A, payoff_player_B]
 
     def run(self, player_A, player_B):
-        for turn in range(self.N):
-            move_A = player_A.get_action(self.history[:turn, 1])
-            move_B = player_B.get_action(self.history[:turn, 0])
-            outcome = self._get_outcome(move_A, move_B)
-            self.history[turn, 0] = move_A
-            self.history[turn, 1] = move_B
-            self.history[turn, 2] = outcome
+        history = np.nan * np.ones((self.N, 2))
+        outcomes = np.nan * np.ones((self.N, ))
+        payoff = np.nan * np.ones((self.N, 2))
 
-        self._compute_payoff(player_A, player_B)
+        for turn in range(self.N):
+            move_A = player_A.get_action(turn, history, payoff)
+            move_B = player_B.get_action(turn,
+                                         np.flip(history, axis=1),
+                                         np.flip(payoff, axis=1))
+            outcome = self._get_outcome(move_A, move_B)
+            history[turn, 0] = move_A
+            history[turn, 1] = move_B
+
+            outcomes[turn] = outcome
+            payoff[turn] = self._compute_payoff(outcomes[:turn])
+
+        return history, outcomes, payoff
